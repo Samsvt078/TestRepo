a1a9808d94cde101da3049df80ec0a904bcf814f
Samsvt078 <samvenhellsing@gmail.com>
2026-05-07 18:39:34 +0530

    Delete testing-framework-project directory

--- testing-framework-project/features/sample.feature
+++
@@ -1,6 +0,0 @@
-Feature: Sample Feature
-
-  Scenario: Sample Scenario
-    Given Open website
-    When Click on login button
-    Then Enter username and password
--- testing-framework-project/stepDefinitions/Base/BrowserPages.ts
+++
@@ -1,9 +0,0 @@
-export interface BrowserContext {
-    browser: any;
-    page: any;
-}
-
-export const browserContext: BrowserContext = {
-    browser: null,
-    page: null
-};
